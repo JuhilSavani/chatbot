ab3d511f86e250c9db29da1f540f01c72b783fb6
JuhilSavani <savanijuhil@gmail.com>
2026-02-17 07:28:18 +0530

    Fix incorrect base url

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -4,6 +4,6 @@ let BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:4000";
 BASE_URL = BASE_URL.endsWith('/') ? BASE_URL.slice(0, -1) : BASE_URL;
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL + '/api',
   withCredentials: true,
 });
